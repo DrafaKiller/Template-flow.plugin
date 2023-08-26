218274574f318697473a1f3f301377c8f5cd95b5
JUST A SNIPER ツ <42767829+DrafaKiller@users.noreply.github.com>
2023-08-26 15:59:35 +0100

    Fixed typescript config & Added better starting example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('This is a template for a TypeScript project.');
+import { Flow } from "flow-plugin";
+
+const flow = new Flow({ keepOrder: true, icon: "app.png" });
+
+flow.add({
+  title: "Welcome to Flow Launcher!",
+  subtitle: "Create your own plugin with Typescript!",
+  jsonRPCAction: Flow.Actions.changeQuery("- Hello World!"),
+});
+
+flow.on("query", ({ prompt }, response) => {
+  response.add({
+    title: "Hello World!",
+    subtitle: `You searched for "${prompt}"`,
+  });
+});
